401d19da7becb9969e2ae49afa60c5b354e30dce
Edoardo Wijaya Grappolini <ewgrappolini@gmail.com>
2023-10-01 11:38:23 +0200

    request interface definition

--- src/main/java/com/caribu/richiesta_orm/data/RequestRepository.java
+++ src/main/java/com/caribu/richiesta_orm/data/RequestRepository.java
@@ -1,5 +1,16 @@
 package com.caribu.richiesta_orm.data;
 
+import java.util.Optional;
+
+import com.caribu.richiesta_orm.model.RequestDTO;
+import com.caribu.richiesta_orm.model.RequestsList;
+
+import io.vertx.core.Future;
+
 public interface RequestRepository {
-    
+    Future<RequestDTO> createRequest(RequestDTO requestDTO);
+    Future<RequestDTO> updateRequest(RequestDTO requestDTO);
+    Future<Optional<RequestDTO>> findRequestById(Integer id);
+    Future<Void> removeRequest(Integer id);
+    Future<RequestsList> findRequestByClientId(Integer clientId);
 }
